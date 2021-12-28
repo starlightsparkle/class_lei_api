1f5a95c281e61cf5355e2c7881cabeab31ab2cce
597751447 <597751447@qq.com>
2021-12-28 18:31:19 +0800

    fix：1.签到标题显示bug，2.结束签到提示更改

--- src/main/java/com/znjz/class_lei/common/entities/TblUserSign.java
+++ src/main/java/com/znjz/class_lei/common/entities/TblUserSign.java
@@ -12,5 +12,6 @@ public class TblUserSign implements Serializable {
     private static final long serialVersionUID = 1L;
     private String locationXy;
     private String username;
+    private Long  userId;
 
 }
